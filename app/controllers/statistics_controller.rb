be78fe4e1c63abbd479bc5f503a016c0f573dac7
Raquel Rodriguez <rodriguez.reads@gmail.com>
2015-04-01 11:24:55 -0500

    added highcharts to pipeline and copied example charts

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -112,6 +112,77 @@ class StatisticsController < ApplicationController
        "DATA:"
        p @player_game_log_data
 
+       #highchart test
+       @bar = LazyHighCharts::HighChart.new('column') do |f|
+           f.series(:name=>'John',:data=> [3, 20, 3, 5, 4, 10, 12 ])
+           f.series(:name=>'Jane',:data=>[1, 3, 4, 3, 3, 5, 4, 20] )     
+           f.title({ :text=>"example test title from controller"})
+
+           ###  Options for Bar
+           ### f.options[:chart][:defaultSeriesType] = "bar"
+           ### f.plot_options({:series=>{:stacking=>"normal"}}) 
+
+           ## or options for column
+           f.options[:chart][:defaultSeriesType] = "column"
+           f.plot_options({:column=>{:stacking=>"normal"}})
+         end
+
+        #highchart test 2
+           @chart = LazyHighCharts::HighChart.new('basic_line') do |f|
+             f.chart({ type: 'line',
+                       marginRight: 130,
+                       marginBottom: 25 })
+             f.title({  text: 'Monthly Average Temperature',
+                        x: -20
+             })
+             f.xAxis({
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                                 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+             })
+             f.yAxis({
+               title: {
+                 text: 'Temperature (°C)'
+               },
+               plotLines: [{
+                 value: 0,
+                 width: 1,
+                 color: '#808080'
+               }]
+             })
+             f.tooltip({
+               valueSuffix: '°C'
+             })
+             f.legend({
+               layout: 'vertical',
+               align: 'right',
+               verticalAlign: 'top',
+               x: -10,
+               y: 100,
+               borderWidth: 0
+             })
+             f.subtitle({
+               text: 'Source: WorldClimate.com',
+               x: -20
+             })
+             f.series({
+               name: 'Tokyo',
+               data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+             })
+             f.series(
+               name: 'New York',
+               data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+             )
+             f.series({
+               name: 'Berlin',
+               data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+             })
+             f.series({
+                 name: 'London',
+                 data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+             })
+           end
+         
+
   end #player
 
   def team
